46bece4ab2917096bc6873d3b8c4b631a7757cd5
opisnerf <98976190+opisnerf@users.noreply.github.com>
2022-09-16 12:35:08 +0300

    Update KohauHibachi.lua

--- KohauHibachi.lua
+++ KohauHibachi.lua
@@ -12,24 +12,17 @@ Tab:AddToggle({
 	Default = false,
 	Callback = function(toggle)
 		while toggle do
-            for i,v in pairs(workspace.Assets.Spills:GetChildren()) do
-                if v and v.Name == "Spill" and toggle then
-                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1593, -315, -1364)
-                    local ohNumber1 = 1
-                    local ohInstance2 = v
-                    local ohString3 = "Mop"
-                    game:GetService("ReplicatedStorage").Packages.Knit.Services.InteractionService.RF.Clicked:InvokeServer(ohNumber1, ohInstance2, ohString3)
-                    print(v, "done")
-                    task.wait()
-                end
-            end
-        end
+            		for i,v in pairs(workspace.Assets.Spills:GetChildren()) do
+                		if v and v.Name == "Spill" then
+                    			local ohNumber1 = 1
+                    			local ohInstance2 = v
+                    			local ohString3 = "Mop"
+                    			game:GetService("ReplicatedStorage").Packages.Knit.Services.InteractionService.RF.Clicked:InvokeServer(ohNumber1, ohInstance2, ohString3)
+                    			task.wait()
+                    			print(v, "done")
+                		end
+           		end
+            		task.wait(0.1)
+        	end
 	end    
-})
-
-Tab:AddButton({
-	Name = "Reset Character",
-	Callback = function()
-      	game.Players.LocalPlayer.Character:BreakJoints()
-  	end    
 })
