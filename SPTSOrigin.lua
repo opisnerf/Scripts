7a0aa70c226c1e95d00884c0b99e42f2f73c7e21
opisnerf <98976190+opisnerf@users.noreply.github.com>
2022-10-17 14:27:56 +0000

    Update SPTSOrigin.lua

--- SPTSOrigin.lua
+++ SPTSOrigin.lua
@@ -2,11 +2,25 @@ local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shl
 local Window = OrionLib:MakeWindow({Name = "SPTS: Origin", HidePremium = true, SaveConfig = true, ConfigFolder = "SPTSOriginScript"})
 
 getgenv().autoPunchToggle = false
+getgenv().bodyZone = 1
 
 local Tab = Window:MakeTab({
 	Name = "Autofarm",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
+})
+
+Tab:AddLabel("This GUI is made to make you look legit (because i have a scripting skill issue)")
+
+Tab:AddLabel("You have to walk to the training zone manually")
+
+Tab:AddDropdown({
+	Name = "Select FS training zone, 1 for none, 2 for large rock",
+	Default = "1",
+	Options = {"1", "2"},
+	Callback = function(Value)
+		bodyZone = Value
+	end    
 })
 
 Tab:AddToggle({
@@ -23,7 +37,7 @@ Tab:AddToggle({
 function autoPunch()
     spawn(function()
         while autoPunchToggle do
-            local ohTable1 = {[1] = "Add_FS_Request", [2] = 1}
+            local ohTable1 = {[1] = "Add_FS_Request", [2] = bodyZone}
             game:GetService("ReplicatedStorage").RemoteEvent:FireServer(ohTable1)
             task.wait()
         end
